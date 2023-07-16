de8cc9795c87f17b73d78a07ce80f929213912f5
Grundig <sevgitr_@hotmail.com>
2023-07-16 21:00:41 +0300

    login design & icons

--- lib/pages/loginPage.dart
+++ lib/pages/loginPage.dart
@@ -1,39 +1,130 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_application_4/components/my_button.dart';
+import 'package:flutter_application_4/components/my_textfield.dart';
 
 class LoginPage extends StatelessWidget {
-  const LoginPage({super.key});
+  LoginPage({super.key});
+
+  //text editing controllers
+  final usernameController = TextEditingController();
+  final passwordController = TextEditingController();
+
+  //sign user in method
+  void signUserIn(){
+
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.grey[300],
       body: SafeArea(
-        child: Column(children: [
-          //logo
-          Icon(
-            Icons.lock,
-            size: 100,
-      
-          )
-      
-      
-          //Welcome back :))
-      
-          //username textfield
-      
-          //password textfield
-      
-          //forgot password
-      
-          //sign in button 
-      
-          //or continue with
-      
-          //google + apple sign in buttons
-      
-          //not a remember? register now
-      
-        ],),
+        child: Center(
+          child: Column(
+            children: [
+              const SizedBox(height: 50),
+
+            //logo
+            const Icon(
+              Icons.lock,
+              size: 100,
+            ),
+
+            const SizedBox(height: 50),
+              
+            //Welcome
+            Text(
+              'Welcome back :))',
+               style: TextStyle(color:Colors.grey[700],
+               fontSize: 16,
+               ),
+            ),
+
+            const SizedBox(height: 25),
+              
+            //username textfield
+            MyTextField(
+              controller: usernameController,
+              hintText: 'Username',
+              obscureText: false,
+            ),
+
+            const SizedBox(height: 10),
+              
+            //password textfield
+            MyTextField(
+              controller: passwordController,
+              hintText: 'Password',
+              obscureText: true,
+               
+            ),
+
+             const SizedBox(height: 10),
+             
+             //forgot password?
+             Padding(
+               padding: const EdgeInsets.symmetric(horizontal: 25.0),
+               child: Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                 children: [
+                   Text('Forgot Password?',
+                   style: TextStyle(color: Colors.grey[600]),
+                   ),
+                 ],
+               ),
+             ),
+
+             const SizedBox(height: 25),
+              
+            //sign in button 
+            MyButton(
+              onTap: signUserIn,
+            ),
+
+            const SizedBox(height: 50),
+              
+            //or continue with
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 25.0),
+              child: Row(
+                children: [
+                  Expanded(
+                    child: Divider(
+                    thickness: 0.5,
+                    color: Colors.grey[400],
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 10.0),
+                    child: Text('Or continue with',
+                    style: TextStyle(color: Colors.grey[700]),
+                    ),
+                  ),
+                  Expanded(
+                    child: Divider(
+                    thickness: 0.5,
+                    color: Colors.grey[400],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+              
+            //google + apple sign in buttons
+           Row(
+            children: [
+              //google button
+              Image.asset('lin/images/google.png'),
+
+
+            ],
+           )
+
+            //not a remember? register now
+              
+          ],
+          ),
+        ),
       ),
     );
   }
