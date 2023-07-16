c0f802e82b3e0753e5776b1de230b5cd89d3586d
Grundig <sevgitr_@hotmail.com>
2023-07-16 13:32:10 +0300

    added login page

---
+++ lib/pages/loginPage.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatelessWidget {
+  const LoginPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey[300],
+      body: Column(children: [
+        //logo
+
+
+        //Welcome back :))
+
+        //username textfield
+
+        //password textfield
+
+        //forgot password
+
+        //sign in button 
+
+        //or continue with
+
+        //google + apple sign in
+
+      ],),
+    );
+  }
+}
