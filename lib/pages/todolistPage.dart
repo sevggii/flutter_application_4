24d8fa1e89bfaf0ff6859602ea2cdf65e2bcdade
Grundig <sevgitr_@hotmail.com>
2023-07-17 19:18:04 +0300

    todoList design finished

--- lib/pages/todolistPage.dart
+++ lib/pages/todolistPage.dart
@@ -1,29 +1,24 @@
 import 'package:flutter/material.dart';
 
+class Task {
+  final String name;
+  bool isCompleted;
+
+  Task({required this.name, this.isCompleted = false});
+}
+
 class ToDoListPage extends StatefulWidget {
   const ToDoListPage({Key? key});
+
   @override
   _ToDoListPageState createState() => _ToDoListPageState();
 }
 
 class _ToDoListPageState extends State<ToDoListPage> {
-  List<String> tasks = [];
-  TextEditingController taskController = TextEditingController();
-
-  void addTask(String task) {
-    setState(() {
-      tasks.add(task);
-      taskController.clear(); // Clear entry area
-    });
-  }
-
-  void completeTask(int index) {
-    setState(() {
-      tasks.removeAt(index);
-    });
-  }
+  List<Task> tasks = [];
 
    Widget build(BuildContext context) {
+     TextEditingController taskController = TextEditingController();
 
      return Scaffold(
        appBar: AppBar(title: Text('To-Do List')),
@@ -32,29 +27,43 @@ class _ToDoListPageState extends State<ToDoListPage> {
            Expanded(
              child: ListView.builder(
                itemCount: tasks.length,
-               itemBuilder: (context, index) => ListTile(
-                 title: Text(tasks[index]),
-                 trailing:
-                     IconButton(icon: Icon(Icons.delete), onPressed: () => completeTask(index)),
-               ),
-             ),
-           ),
-           Padding(
-             padding:
-                 const EdgeInsets.symmetric(horizontal:16.0),
-             child : Row(children:[
-              Expanded(child : TextField(controller :taskController)), //It uses the TextField widget for task entry.
-              IconButton(icon :Icon(Icons.add),onPressed :()=>addTask(taskController.text)) //add task
+               itemBuilder:(context,index){
+                 return ListTile(
+                   leading :Checkbox(
+                     value :tasks[index].isCompleted ?? false, // Add null check and default value
+                     onChanged :(value)=>setState((){tasks[index].isCompleted = value ?? false;}), // Add null check and default value
+                   ),
+                   title :Text(tasks[index].name,
+                     style: TextStyle(decoration:
+                       tasks[index].isCompleted ? TextDecoration.lineThrough : null),
+                   ),
 
-            ])
-           
-           
-          )
-        ],
-      )
-      
-        
-       
-     );
-   }
+                   trailing:
+                       IconButton(icon :Icon(Icons.delete), onPressed :()=>setState(()=>tasks.removeAt(index))),
+                 );
+                }
+              ),
+            ),
+            Padding(padding :
+              const EdgeInsets.symmetric(horizontal :16.0),
+              child :
+                Row(children:[
+                  Expanded(child:
+                    TextField(
+                      controller: taskController,
+                      onSubmitted: (value) => setState(() { 
+                        tasks.add(Task(name:value));
+                        }),
+                    ),
+                  ),
+                  IconButton(icon :Icon(Icons.add), onPressed :()=> setState(() { 
+                      tasks.add(Task(name:taskController.text));
+                      taskController.clear();
+                    }))
+                ])
+            )
+          ],
+        ),
+      );
+    }
 }
