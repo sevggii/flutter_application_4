70118c3604f85ba097ab999aea0cbc04b9488e8b
Grundig <sevgitr_@hotmail.com>
2023-07-18 16:40:10 +0300

    git problem solved

--- lib/pages/loginPage.dart
+++ lib/pages/loginPage.dart
@@ -7,7 +7,7 @@ import 'package:http/http.dart' as http;
 
 
 class LoginPage extends StatefulWidget {
-  const LoginPage({Key? key});
+  const LoginPage({Key? key});//git control 1 2 3
 
   @override
   _LoginPageState createState() => _LoginPageState();
