3821128378692a077ddb5a0e86e8d638eb50d951
Grundig <sevgitr_@hotmail.com>
2023-07-18 17:00:07 +0300

    edit

--- lib/pages/loginPage.dart
+++ lib/pages/loginPage.dart
@@ -7,7 +7,7 @@ import 'package:http/http.dart' as http;
 
 
 class LoginPage extends StatefulWidget {
-  const LoginPage({Key? key});//git control 1 2 3
+  const LoginPage({Key? key});
 
   @override
   _LoginPageState createState() => _LoginPageState();
